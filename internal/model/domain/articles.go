2681ecd28e0b0760a11d164ef2c63be1ed1362d4
arya <praba.elmahdi@gmail.com>
2024-05-20 14:41:33 +0700

    fix: fixed typo "validae"

--- internal/model/domain/articles.go
+++ internal/model/domain/articles.go
@@ -5,7 +5,7 @@ import "time"
 type Articles struct {
 	ArticleID uint      `gorm:"type:int;autoIncrement;primaryKey" json:"articleID"`
 	SoilType  SoilTypes `gorm:"type:enum('Aluvial','Andosol','Latosol','Humus');not null" json:"soilType" validate:"required"`
-	Title     string    `gorm:"type:varchar(50);not null" json:"title" validae:"required"`
+	Title     string    `gorm:"type:varchar(50);not null" json:"title" validate:"required"`
 	Content   string    `gorm:"type:text;not null" json:"content" validate:"required"`
 	CreatedAt time.Time `gorm:"autoCreateTime" json:"createdAt"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updatedAt"`
